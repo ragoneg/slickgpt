bd0256f59ad2eef31383c6b9a95f6ff9d9e7e3b2
beppe <ragoneg@gmail.com>
2023-11-14 19:17:54 +0800

    test-pwa1

--- vite.config.ts
+++ vite.config.ts
@@ -10,40 +10,44 @@ export default defineConfig({
 	},
 	plugins: [sveltekit(), SvelteKitPWA({
 		registerType: "autoUpdate",
+		injectRegister: 'auto',
 		strategies: 'generateSW',
 		srcDir: 'src',
 		scope: '/',
 		base: '/',
 		selfDestroying: process.env.SELF_DESTROYING_SW === 'true',
-		manifest: {
-			short_name: 'GPT PWA',
-			name: 'GPT PWA',
-			start_url: '/',
-			scope: '/',
-			display: 'standalone',
-			theme_color: "#ffffff",
-			background_color: "#ffffff",
-			icons: [
-				{
-					src: '/pwa-192x192.png',
-					sizes: '192x192',
-					type: 'image/png',
-				},
-				{
-					src: '/pwa-512x512.png',
-					sizes: '512x512',
-					type: 'image/png',
-				},
-				{
-					src: '/pwa-512x512.png',
-					sizes: '512x512',
-					type: 'image/png',
-					purpose: 'any maskable',
-				},
-			],
-		},
+		// manifest: {
+		// 	short_name: 'GPT PWA',
+		// 	name: 'GPT PWA',
+		// 	start_url: '/',
+		// 	scope: '/',
+		// 	display: 'standalone',
+		// 	theme_color: "#ffffff",
+		// 	background_color: "#ffffff",
+		// 	icons: [
+		// 		{
+		// 			src: '/pwa-192x192.png',
+		// 			sizes: '192x192',
+		// 			type: 'image/png',
+		// 		},
+		// 		{
+		// 			src: '/pwa-512x512.png',
+		// 			sizes: '512x512',
+		// 			type: 'image/png',
+		// 		},
+		// 		{
+		// 			src: '/pwa-512x512.png',
+		// 			sizes: '512x512',
+		// 			type: 'image/png',
+		// 			purpose: 'any maskable',
+		// 		},
+		// 	],
+		// },
+		// injectManifest: {
+		// 	globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}']
+		// },
 		injectManifest: {
-			globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}']
+			injectionPoint: undefined
 		},
 		workbox: {
 			globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}'],
